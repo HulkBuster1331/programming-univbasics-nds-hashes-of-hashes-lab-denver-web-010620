0c6be6a7ca518af2b7d578cbd9a4d7c9972f0128
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-21 00:36:53 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -62,13 +62,4 @@ def animal_kingdom
   
   
   
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
+ 
