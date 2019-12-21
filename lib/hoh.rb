b1ecb57dbb2029a061a9a504f922522018d86431
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-21 00:14:53 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -30,20 +30,18 @@
 #   sub_category: nil
 # }
 
-def naming_system
-      
-      kingdom = {
+kingdom = {
    label: "Kingdom",
      sub_category: {
          label: "Phylum",
       sub_category: {
-           label: "Class", {
+           label: "Class", 
           sub_category: {   
-             label: "Order", {
+             label: "Order", 
            sub_category: {
                label: "Family",
            sub_category: {
-                 label: "Genus", {
+                 label: "Genus", 
               sub_category: {     
                    label: "Species",
                  sub_category: nil
@@ -54,11 +52,7 @@ def naming_system
             }
            }
           }
-        
-      
-      
-    end
-           
+         end
   
   
   
